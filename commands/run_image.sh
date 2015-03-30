9857609abf8c3ebe053a26252216b5cadaca9d0c
Thiago Rodrigues <thiagorsouz@gmail.com>
2015-03-30 05:00:44 -0300

    initial update to run_image using docker-compose

--- commands/run_image.sh
+++ commands/run_image.sh
@@ -2,13 +2,32 @@
 
 function main_run_image {
     shift
-    local name=$1
-    local image=$2
-    shift; shift
-    if [ "${1-}" == "d" ]; then
-        shift
-        docker run --rm -ti -v $(pwd):$(pwd) -w $(pwd) $image /bin/sh $@
+    local name=$(get_image_name $1)
+    if [ "$(echo $2 | head -c 1)" == ":" ]; then
+        local version=$(get_image_version $2)
     else
-        docker run --rm -ti -v $(pwd):$(pwd) -w $(pwd) $image $(get_image_name $name) $@
+        local version=$(get_image_version $1)
     fi
+    version=${version//./""}
+    version=${version//-/""}
+    local args_length=$(($#))
+    local args_array=${@:2:args_length}
+
+    TMPL_FILE="$PROJECT_DIR/installed/$name.yml"
+    TMPL_ENV="$PROJECT_DIR/installed/$name.env"
+    TMPL_ENV_COMMON="$PROJECT_DIR/installed/common.env"
+    TMPL_TMP="TMPL_CURRENT=\"`echo -e "$(cat $TMPL_FILE)"`\""
+
+    if [ -f "${TMPL_ENV_COMMON}" ]; then
+        eval $(cat $TMPL_ENV_COMMON)
+    fi
+
+    if [ -f "${TMPL_ENV}" ]; then
+        eval $(cat $TMPL_ENV)
+    fi
+
+    eval "$TMPL_TMP"
+    echo -e "$TMPL_CURRENT" > $TMP_DIR/$name.yml
+
+    docker-compose -f $TMP_DIR/$name.yml -p $(pwd) run --rm $name$version $args_array
 }
