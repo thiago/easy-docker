bc156fccefba15f9e8a368a92390afa8fd68e902
Thiago Rodrigues <thiagorsouz@gmail.com>
2015-06-05 22:54:04 -0300

    improving alias and ls commands

--- commands/ls.sh
+++ commands/ls.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
 function main_ls {
-	grep -nr META* $BIN_DIR | awk '{print $3, $4}'
+
+    local IFS=$'\n'
+    local list=( `grep -nr "$PROJECT_DIR/main.sh" $BIN_DIR` )
+    local alias=""
+    local command=""
+
+    echo -e ""
+    echo -e " ALIAS\t\tCOMMAND"
+    echo -e " -------------- ---------------------------------------------"
+    for (( i = 0; i < ${#list[@]}; i++ )); do
+        alias=`echo "${list[$i]}" | \
+            awk -F"$PROJECT_DIR/main.sh run" '{print $1}' | \
+            awk -F':' '{print $1}' | \
+            awk -F"$BIN_DIR/" '{print $2}'`
+
+        command=`echo "${list[$i]}" | awk -F"$PROJECT_DIR/main.sh run" '{print $2}'`
+
+        echo -e " $(title $alias)\t\t$command"
+    done
+    echo -e ""
 }
